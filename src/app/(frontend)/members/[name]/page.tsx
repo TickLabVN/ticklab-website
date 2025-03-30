393046ab78512a630e3f9d1f06e52a6754f828e9
Gia Bao <thainguyengiabao27092004@gmail.com>
2025-03-31 00:54:35 +0700

    feat: add member list and profile components with client-side header theme management

---
+++ src/app/(frontend)/members/[name]/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageClient from './page.client'
+import { Button } from 'flowbite-react'
+function MemberProfile() {
+  return (
+    <div>
+      <PageClient />
+      <Button>heh</Button>
+      MemberProfile
+    </div>
+  )
+}
+
+export default MemberProfile
---
+++ src/app/(frontend)/members/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PageClient from './page.client'
+
+function MemberList() {
+  return (
+    <div>
+      <PageClient />
+      MemberList
+    </div>
+  )
+}
+
+export default MemberList
